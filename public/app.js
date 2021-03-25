ac369e02ec2ac23b09065b4302cea226b6531537
Manuel-m <discoride.dev@gmail.com>
2021-03-25 23:46:33 +0100

    train mark

--- public/app.js
+++ public/app.js
@@ -15248,17 +15248,37 @@
     return shuffled.slice(0, size);
   }
 
+  const trainItems = [];
+
+  function genTrainItems() {
+    const values = sample(Object.keys(items).map((k) => [k, ...items[k]]), 10);
+    const shuffled = values.map((v) => [
+      v,
+      Math.floor(Math.random() * 3),
+      '', // answer
+      '', // mark
+    ]);
+    for (let i = 0; i < shuffled.length; i++) {
+      trainItems[i] = shuffled[i];
+    }
+  }
+
   const trainComp = {
     data() {
-      const values = sample(Object.keys(items).map((k) => [k, ...items[k]]), 10);
-      const shuffled = values.map((v) => [v, Math.floor(Math.random() * 3), '']);
+      if (trainItems.length === 0) {
+        genTrainItems();
+      }
       return {
-        items: shuffled,
+        trainItems,
       };
     },
     methods: {
       check(e) {
-        console.log(this.items);
+        for (const trainItem of trainItems) {
+          const [values, fi, answer] = trainItem;
+          trainItem[3] = values[fi] === answer ? 'ok' : 'ko';
+        }
+        console.log(trainItems);
       },
     },
     template: `<div class="container m-3 mx-auto">
@@ -15276,7 +15296,7 @@
       </div>
   </div>
 
-  <div v-for="o in items" class="flex mt-1 ml-1 bg-gray-100">
+  <div v-for="o in trainItems" class="flex mt-1 ml-1 bg-gray-100">
 
      <input v-model="o[2]" v-if="o[1] === 0" class="p-1 w-1/4 border-2 border-blue-300 rounded">
      <div v-else class="p-1 w-1/4">{{o[0][0]}}</div>
